592704de0b1a297fbafb5d02bdbe055fc14fe89c
moving-line <movinglinecheck@gmail.com>
2019-04-08 21:31:41 +0900

    Update Annotation - context namespace 설정 & commponent-scan 설정 - <bean> element 대신 @Component 활용하여 클래스 등록

--- src/main/java/polymorphism/LgTV.java
+++ src/main/java/polymorphism/LgTV.java
@@ -1,6 +1,13 @@
 package polymorphism;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class LgTV implements TV {
+	public LgTV() {
+		System.out.println("===> LgTV 객체 생성");
+	}
+	
 	public void powerOn() {
 		System.out.println("LgTV--전원 켠다.");
 	}
--- src/main/java/polymorphism/TVUser.java
+++ src/main/java/polymorphism/TVUser.java
@@ -9,7 +9,7 @@ public class TVUser {
 		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
 		
 		// 2. 스프링 컨테이너로부터 필요한 객체를 요청한다.
-		TV tv = (TV) factory.getBean("tv");
+		TV tv = (TV) factory.getBean("TV");
 		
 		tv.powerOn();
 		tv.volumeDown();
