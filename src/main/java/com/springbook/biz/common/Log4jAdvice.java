5c8711bfb9ae4e2b7452faa48a23a92d433ed16c
moving-line <movinglinecheck@gmail.com>
2019-04-10 22:05:00 +0900

    Update AOP
    - JoinPoint Interface 및 바인드 변수 활용

--- src/main/java/com/springbook/biz/common/Log4jAdvice.java
+++ src/main/java/com/springbook/biz/common/Log4jAdvice.java
@@ -1,7 +1,9 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class Log4jAdvice {
-	public void printLogging() {
+	public void printLogging(JoinPoint jp) {
 		System.out.println("[공통 로그-Log4j] 비즈니스 로직 수행 전 동");
 	}
 }
