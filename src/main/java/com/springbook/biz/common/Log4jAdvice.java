36fa998ece7514b21db955de25599977b023c6da
moving-line <movinglinecheck@gmail.com>
2019-04-11 21:58:01 +0900

    Update AOP
    - Annotation 기반으로 변경
    - Pointcut 중복 제거(외부 참조)

--- src/main/java/com/springbook/biz/common/Log4jAdvice.java
+++ src/main/java/com/springbook/biz/common/Log4jAdvice.java
@@ -1,8 +1,18 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
+@Service
+@Aspect
 public class Log4jAdvice {
+	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+	public void allPointcut() {}
+	
+	@Before("allPointcut()")
 	public void printLogging(JoinPoint jp) {
 		System.out.println("[공통 로그-Log4j] 비즈니스 로직 수행 전 동");
 	}
