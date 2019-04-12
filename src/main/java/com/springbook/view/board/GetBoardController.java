9bf2a24062a6a0ca13c571a49fcaba97388ab1fe
moving-line <movinglinecheck@gmail.com>
2019-04-13 01:19:28 +0900

    Spring MVC 적용
    - DispatcherServlet 재설정
    - Encoding 설정(web.xml에 filter 처리)
    - 구현된 Controller들 SpringMVC에 맞게 수정
    - Controller Bean & Mapping 등록(presentation-layer.xml)

--- src/main/java/com/springbook/view/board/GetBoardController.java
+++ src/main/java/com/springbook/view/board/GetBoardController.java
@@ -2,15 +2,16 @@ package com.springbook.view.board;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
-import com.springbook.view.controller.Controller;
 
 public class GetBoardController implements Controller {
 	@Override
-	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println("글 상세 조회 처리 ");
 		
 		// 1. 검색할 게시글 번호 추출 
@@ -22,10 +23,10 @@ public class GetBoardController implements Controller {
 		BoardDAO boardDAO = new BoardDAO();
 		BoardVO board = boardDAO.getBoard(vo);
 		
-		// 3) 검색 결과를 세션에 저장하고, 목록 화면으로 이동한다.
-		HttpSession session = request.getSession();
-		session.setAttribute("board", board);
-		
-		return "getBoard";
+		// 3) 검색 결과와 화면 정보를 ModelAndView에 저장하여 리턴한다.
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("board", board);
+		mav.setViewName("getBoard");
+		return mav;
 	}
 }
--- src/main/java/com/springbook/view/user/LoginController.java
+++ src/main/java/com/springbook/view/user/LoginController.java
@@ -3,13 +3,15 @@ package com.springbook.view.user;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
 import com.springbook.biz.user.UserVO;
 import com.springbook.biz.user.impl.UserDAO;
-import com.springbook.view.controller.Controller;
 
 public class LoginController implements Controller {
 	@Override
-	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println("로그인 처리 ");
 		
 		// 1) 사용자 입력 정보 추출 
@@ -25,7 +27,13 @@ public class LoginController implements Controller {
 		UserVO user = userDAO.getUser(vo);
 		
 		// 3) 화면 네비게이션 
-		if (user != null) return "getBoardList.do";
-		else return "login";
+		ModelAndView mav = new ModelAndView();
+		if (user != null) {
+			mav.setViewName("redirect:getBoardList.do");
+		}
+		else {
+			mav.setViewName("redirect:login.jsp");
+		}
+		return mav;
 	}
 }
