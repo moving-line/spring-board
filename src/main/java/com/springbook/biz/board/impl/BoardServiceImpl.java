a3ca6695c679c48531750524ca47bf667a03ffa5
moving-line <movinglinecheck@gmail.com>
2019-04-13 22:58:33 +0900

    Presentation Layer - Business Layer 통합
    - Controller의 DAO 직접 호출 구조 변경(using Service Interface)
    - Business Component Loading 위해 ContextLoaderListener 등록
    - etc(검색 기능 구현)

--- src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -11,13 +11,12 @@ import com.springbook.biz.board.BoardVO;
 @Service
 public class BoardServiceImpl implements BoardService{
 	@Autowired
-	private BoardDAOSpring boardDAO;
+	private BoardDAO boardDAO;
 
 	public void insertBoard(BoardVO vo) {
 //		if(vo.getSeq() == 0) {
 //			throw new IllegalArgumentException("0번 글은 등록할 수 없습니다.");
 //		}
-		boardDAO.insertBoard(vo);
 		boardDAO.insertBoard(vo);
 	}
 
--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -1,50 +1,63 @@
 package com.springbook.view.board;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
-import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
+@SessionAttributes("board")
 public class BoardController {
+	@Autowired
+	private BoardService boardService;
 	
 	@RequestMapping("/insertBoard.do")
-	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
-		boardDAO.insertBoard(vo);
-		
+	public String insertBoard (BoardVO vo) {
+		boardService.insertBoard(vo);
 		return "redirect:getBoardList.do";
 	}
 	
 	@RequestMapping("/updateBoard.do")
-	public String updateBoard(BoardVO vo, BoardDAO boardDAO) {
-		boardDAO.updateBoard(vo);
-		
+	public String updateBoard(@ModelAttribute("board") BoardVO vo) {
+		boardService.updateBoard(vo);
 		return "getBoardList.do";
 	}
 	
 	@RequestMapping("/deleteBoard.do")
-	public String deleteBoard(BoardVO vo, BoardDAO boardDAO) {
-		boardDAO.deleteBoard(vo);
-
+	public String deleteBoard(BoardVO vo) {
+		boardService.deleteBoard(vo);
 		return "getBoardList.do";
 	}
 	
 	@RequestMapping("/getBoard.do")
-	public ModelAndView getBoard(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
-		mav.addObject("board", boardDAO.getBoard(vo));
-		mav.setViewName("getBoard.jsp");
-		
-		return mav;
+	public String getBoard(BoardVO vo, Model model) {
+		model.addAttribute("board", boardService.getBoard(vo));
+		return "getBoard.jsp";
+	}
+	
+	@ModelAttribute("conditionMap")
+	public Map<String, String> searchConditionMap() {
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("제목", "TITLE");
+		conditionMap.put("내용", "CONTENT");
+		return conditionMap;
 	}
 	
 	@RequestMapping("/getBoardList.do")
-	public ModelAndView getBoardList(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
-		mav.addObject("boardList", boardDAO.getBoardList(vo));
-		mav.setViewName("getBoardList.jsp");
+	public String getBoardList(BoardVO vo, Model model) {		
+		if(vo.getSearchCondition() == null) vo.setSearchCondition("TITLE");
+		if(vo.getSearchKeyword() == null) vo.setSearchKeyword("");
 		
-		return mav;
+		model.addAttribute("boardList", boardService.getBoardList(vo));
+		return "getBoardList.jsp";
 	}
 
 }
